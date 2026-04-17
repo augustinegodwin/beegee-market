8392cf94d8d94349656e9c6a728eb021388b993e
100abrahamemmanuel <ewelaabraham2007@gmail.com>
2026-04-16 19:58:51 -0700

    chore: move secrets to env and clean modal

--- app/components/utils/sectionHeader.tsx
+++ app/components/utils/sectionHeader.tsx
@@ -12,7 +12,7 @@ export default function SectionHeader(data: SHProp) {
       <div className="w-full items-center justify-center max-w-150 flex flex-col gap-5">
         <div className="w-fit">
         <h2 className="text-center custom4 text-5xl sm:text-6xl lg:text-[75px] text-(--primary) leading-none tracking-header">
-
+          {data.title}
           </h2>
         </div>
         <div className="w-full max-w-100 flex flex-col justify-center items-center gap-5">
